0c0f8eebd362ca05c62acb64a0172744410294ba
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-11-25 15:05:30 +0300

    upd

--- src/main/java/com/example/UMS/Repository/UserRepository.java
+++ src/main/java/com/example/UMS/Repository/UserRepository.java
@@ -5,11 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
 
     User findUserById(Integer id);
-    @Query()
+    User findUserByEmail(String email);
+    List<User> findUserByRole(String role);
+    @Query("SELECT ")
+    List<User> getUsersByAge(Integer range);
+
+    @Query("")
     User logIn(String username,String password);
 
 }
